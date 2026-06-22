776175da49eb077d27c279d2137c2463bfd01207
Umoru Sarah <sarahumoru123@gmail.com>
2026-06-22 07:25:09 +0100

    gitignore added

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-AIzaSyAEa9BtvYs2RjSvRrZZLb2Pz3P-9Xfo5jI
+const firebaseConfig = {
+  apiKey: "AIzaSyAEa9BtvYs2RjSvRrZZLb2Pz3P-9Xfo5jI",
+  authDomain: "nova-smile-404b5.firebaseapp.com",
+  projectId: "nova-smile-404b5",
+  storageBucket: "nova-smile-404b5.firebasestorage.app",
+  messagingSenderId: "205119413201",
+  appId: "1:205119413201:web:3e7027c9bf16978206def0"
+};
